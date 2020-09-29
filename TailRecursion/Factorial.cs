4451957b6c03e3e7aa79b23c2f7f7c0f64f94d9c
Ramin Anvar <ramina1964@gmail.com>
2020-09-29 16:32:09 +0200

    Add project files.

---
+++ TailRecursion/Factorial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TailRecursive.Model
+{
+    public class Factorial
+    {
+        /**************************** Constants ******************************/
+        public const int MinInput = 0;
+        public const int MaxInput = 25;
+
+        /*************************** Constructors ****************************/
+        public Factorial(int input)
+        {
+            InputValue = input;
+        }
+
+        /**************************** Properties *****************************/
+        public int InputValue
+        {
+            get { return _inputValue; }
+
+            set
+            {
+                if (MinInput > value || value > MaxInput)
+                {
+                    var msg = "Input argument must be in the range: " + $"[{MinInput}, {MaxInput}";
+                    throw new ArgumentOutOfRangeException(msg);
+                }
+
+                _inputValue = value;
+            }
+        }
+
+        /************************** Public Methods ***************************/
+        public override string ToString()
+        {
+            var factList = Enumerable.Range(0, InputValue + 1)
+                                     .Select(item => CalculateFactorial(item))
+                                     .ToList();
+
+            var sb = new StringBuilder().AppendLine("Factorial:");
+            for (var i = MinInput; i <= InputValue; i++)
+            {
+                var result = factList[i];
+                sb.AppendLine($"{i,2}! = {result,10}");
+            }
+
+            return sb.AppendLine().ToString();
+        }
+
+        /************************** Private Methods **************************/
+        public long CalculateFactorial(int nFact)
+        {
+            return CalculateFactorial(nFact, 1);
+        }
+
+        private long CalculateFactorial(int nFact, long acc)
+        {
+            return (nFact < 2) ?
+              acc :
+              CalculateFactorial(nFact - 1, nFact * acc);
+        }
+
+        /************************** Private Fields ***************************/
+        private int _inputValue;
+    }
+
+}
---
+++ TailRecursion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TailRecursive.Model;
+
+namespace TailRecursion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tail Recursive Fibonacci Calculation!");
+            Console.Write("Enter 1 for Factorial, 2 for Fibonacci calculation: ");
+            var choice = Console.ReadLine();
+            if (int.TryParse(choice, out int choiceValue))
+            {
+                if (choiceValue == 1)
+                {
+                    Console.Write("Enter Factorial Input: ");
+                    var facTarget = Console.ReadLine();
+                    if (int.TryParse(facTarget, out int facInput))
+                    {
+                        var fac = new Factorial(facInput);
+                        Console.WriteLine(fac);
+                    }
+                }
+
+                else
+                {
+                    Console.Write("Enter Fibonacci Input: ");
+                    var fibTarget = Console.ReadLine();
+                    if (int.TryParse(fibTarget, out int fibInput))
+                    {
+                        var fib = new Fibonacci(fibInput);
+                        Console.WriteLine(fib);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
