21a76b6ed5914843c5306eeba11bfa0598f31711
Ramin Anvar <ramina1964@gmail.com>
2023-11-04 22:06:41 +0100

    Converted the project to Top Level Staments Moved classes Factorial and Fibonacci into a new folder called Models Moved all usings into a GlobalUsings file

--- TailRecursion/Program.cs
+++ TailRecursion/Program.cs
@@ -1,41 +1,31 @@
-﻿using System;
-using TailRecursive.Model;
-
-namespace TailRecursion
+﻿
+Console.WriteLine("Welcome to Tail Recursive Fibonacci Calculation!");
+Console.Write("Enter 1 for Factorial, 2 for Fibonacci calculation: ");
+var choice = Console.ReadLine();
+if (int.TryParse(choice, out int choiceValue))
 {
-    public class Program
+    if (choiceValue == 1)
     {
-        static void Main(string[] args)
+        Console.Write("Enter Factorial Input: ");
+        var facTarget = Console.ReadLine();
+        if (int.TryParse(facTarget, out int facInput))
         {
-            Console.WriteLine("Welcome to Tail Recursive Fibonacci Calculation!");
-            Console.Write("Enter 1 for Factorial, 2 for Fibonacci calculation: ");
-            var choice = Console.ReadLine();
-            if (int.TryParse(choice, out int choiceValue))
-            {
-                if (choiceValue == 1)
-                {
-                    Console.Write("Enter Factorial Input: ");
-                    var facTarget = Console.ReadLine();
-                    if (int.TryParse(facTarget, out int facInput))
-                    {
-                        var fac = new Factorial(facInput);
-                        Console.WriteLine(fac);
-                    }
-                }
+            var fac = new Factorial(facInput);
+            Console.WriteLine(fac);
+        }
+    }
 
-                else
-                {
-                    Console.Write("Enter Fibonacci Input: ");
-                    var fibTarget = Console.ReadLine();
-                    if (int.TryParse(fibTarget, out int fibInput))
-                    {
-                        var fib = new Fibonacci(fibInput);
-                        Console.WriteLine(fib);
-                    }
-                }
-            }
-
-            Console.ReadLine();
+    else
+    {
+        Console.Write("Enter Fibonacci Input: ");
+        var fibTarget = Console.ReadLine();
+        if (int.TryParse(fibTarget, out int fibInput))
+        {
+            var fib = new Fibonacci(fibInput);
+            Console.WriteLine(fib);
         }
     }
 }
+
+Console.ReadLine();
+
